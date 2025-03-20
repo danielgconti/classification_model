207ab23219ca1b100d43beebb69327e8875d20bd
Daniel Golia Conti <contidan@msu.edu>
2025-03-19 23:49:28 -0400

    Normalized data set

--- index.py
+++ index.py
@@ -32,4 +32,76 @@ rice_dataset = rice_dataset_raw[[
     'Class',
 ]]
 
-rice_dataset.describe()
+print(rice_dataset.describe())
+
+# @title Solutions (run the cell to get the answers)
+
+print(
+    f'The shortest grain is {rice_dataset.Major_Axis_Length.min():.1f}px long,'
+    f' while the longest is {rice_dataset.Major_Axis_Length.max():.1f}px.'
+)
+print(
+    f'The smallest rice grain has an area of {rice_dataset.Area.min()}px, while'
+    f' the largest has an area of {rice_dataset.Area.max()}px.'
+)
+print(
+    'The largest rice grain, with a perimeter of'
+    f' {rice_dataset.Perimeter.max():.1f}px, is'
+    f' ~{(rice_dataset.Perimeter.max() - rice_dataset.Perimeter.mean())/rice_dataset.Perimeter.std():.1f} standard'
+    f' deviations ({rice_dataset.Perimeter.std():.1f}) from the mean'
+    f' ({rice_dataset.Perimeter.mean():.1f}px).'
+)
+print(
+    f'This is calculated as: ({rice_dataset.Perimeter.max():.1f} -'
+    f' {rice_dataset.Perimeter.mean():.1f})/{rice_dataset.Perimeter.std():.1f} ='
+    f' {(rice_dataset.Perimeter.max() - rice_dataset.Perimeter.mean())/rice_dataset.Perimeter.std():.1f}'
+)
+
+# Create five 2D plots of the features against each other, color-coded by class.
+# for x_axis_data, y_axis_data in [
+#     ('Area', 'Eccentricity'),
+#     ('Convex_Area', 'Perimeter'),
+#     ('Major_Axis_Length', 'Minor_Axis_Length'),
+#     ('Perimeter', 'Extent'),
+#     ('Eccentricity', 'Major_Axis_Length'),
+# ]:
+#     print(px.scatter(rice_dataset, x=x_axis_data, y=y_axis_data, color='Class').show())
+
+#@title Plot three features in 3D by entering their names and running this cell
+
+x_axis_data = 'Area'  # @param {type: "string"}
+y_axis_data = 'Eccentricity'  # @param {type: "string"}
+z_axis_data = 'Major_Axis_Length'  # @param {type: "string"}
+
+px.scatter_3d(
+    rice_dataset,
+    x=x_axis_data,
+    y=y_axis_data,
+    z=z_axis_data,
+    color='Class',
+).show()
+
+feature_mean = rice_dataset.mean(numeric_only=True)
+feature_std = rice_dataset.std(numeric_only=True)
+numerical_features = rice_dataset.select_dtypes('number').columns
+normalized_dataset = (
+    rice_dataset[numerical_features] - feature_mean
+) / feature_std
+
+# Copy the class to the new dataframe
+normalized_dataset['Class'] = rice_dataset['Class']
+
+# Examine some of the values of the normalized training set. Notice that most
+# Z-scores fall between -2 and +2.
+print(normalized_dataset.head())
+
+keras.utils.set_random_seed(42)
+
+# Create a column setting the Cammeo label to '1' and the Osmancik label to '0'
+# then show 10 randomly selected rows.
+normalized_dataset['Class_Bool'] = (
+    # Returns true if class is Cammeo, and false if class is Osmancik
+    normalized_dataset['Class'] == 'Cammeo'
+).astype(int)
+
+print(normalized_dataset.sample(10))
