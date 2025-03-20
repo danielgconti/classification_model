445e075252b57e26e4fbaab946ee8daa6afbdc33
Daniel Golia Conti <contidan@msu.edu>
2025-03-19 22:06:46 -0400

    Added index file and created repo

---
+++ index.py
@@ -0,0 +1,20 @@
+# @title Load the imports
+
+import io
+import keras
+from matplotlib import pyplot as plt
+from matplotlib.lines import Line2D
+import ml_edu.experiment
+import ml_edu.results
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+# The following lines adjust the granularity of reporting.
+pd.options.display.max_rows = 10
+pd.options.display.float_format = "{:.1f}".format
+
+print("Ran the import statements.")
+
+# @title Load the dataset
+rice_dataset_raw = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/Rice_Cammeo_Osmancik.csv")
